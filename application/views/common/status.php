7d809e6034528c56edde2545164345ec023c99bd
pujie <pw.prayitno@gmail.com>
2016-11-21 15:14:36 +0700

    separate status from index

---
+++ application/views/common/status.php
@@ -0,0 +1,65 @@
+<ul class="buttons">
+    <li>
+        <a href="#" class="link_bcPopupList">
+        <span class="icon-user"></span><span class="text">Pelanggan Anda</span></a>
+        <div id="bcPopupList" class="popup">
+            <div class="head clearfix">
+                <div class="arrow"></div>
+                <span class="isw-users"></span>
+                <span class="name">Pelanggan Anda</span>
+            </div>
+            <div class="body-fluid users">
+
+                <div class="item clearfix">
+                    <div class="image"><a href="#"><img src="/assets/aquarius/img/users/aqvatarius_s.jpg" width="32"/></a></div>
+                    <div class="info">
+                        <a href="#" class="name">Leads</a>                                    
+                        <span>15</span>
+                    </div>
+                </div>
+
+                <div class="item clearfix">
+                    <div class="image">
+                        <a href="#"><img src="/assets/aquarius/img/users/no-image50.jpg" width="32"/></a>
+                    </div>
+                    <div class="info">
+                        <a href="#" class="name">Prospects</a>                                
+                        <span>12</span>
+                    </div>
+                </div>                        
+
+                <div class="item clearfix">
+                    <div class="image">
+                        <a href="#"><img src="/assets/aquarius/img/users/no-image50.jpg" width="32"/></a>
+                    </div>
+                    <div class="info">
+                        <a href="#" class="name">Survey</a>  
+                        <span>111</span>
+                    </div>
+                </div>                              
+            
+                <div class="item clearfix">
+                    <div class="image">
+                        <a href="#"><img src="/assets/aquarius/img/users/no-image50.jpg" width="32"/></a>
+                    </div>
+                    <div class="info">
+                        <a href="#" class="name">Install</a>                                    
+                        <span>1122</span>
+                    </div>
+                </div>
+                <div class="item clearfix">
+                    <div class="image">
+                        <a href="#"><img src="/assets/aquarius/img/users/no-image50.jpg" width="32"/></a>
+                    </div>
+                    <div class="info">
+                        <a href="#" class="name">Disconnect</a>
+                        <span>11</span>
+                    </div>
+                </div>
+            </div>
+            <div class="footer">
+                <button class="btn btn-danger link_bcPopupList" type="button">Close</button>
+            </div>
+        </div>
+    </li>
+</ul>
--- application/views/prospects/index.php
+++ application/views/prospects/index.php
@@ -19,71 +19,7 @@
                 <li><a href="/">PadiApp</a> <span class="divider">></span></li>     
                 <li class="active">Prospect</li>
             </ul>
-            <ul class="buttons">
-                <li>
-                    <a href="#" class="link_bcPopupList">
-                    <span class="icon-user"></span><span class="text">Pelanggan Anda</span></a>
-                    <div id="bcPopupList" class="popup">
-                        <div class="head clearfix">
-                            <div class="arrow"></div>
-                            <span class="isw-users"></span>
-                            <span class="name">Pelanggan Anda</span>
-                        </div>
-                        <div class="body-fluid users">
-
-                            <div class="item clearfix">
-                                <div class="image"><a href="#"><img src="/assets/aquarius/img/users/aqvatarius_s.jpg" width="32"/></a></div>
-                                <div class="info">
-                                    <a href="#" class="name">Leads</a>                                    
-                                    <span>5</span>
-                                </div>
-                            </div>
-
-                            <div class="item clearfix">
-                                <div class="image">
-                                    <a href="#"><img src="/assets/aquarius/img/users/no-image50.jpg" width="32"/></a>
-                                </div>
-                                <div class="info">
-                                    <a href="#" class="name">Prospects</a>                                
-                                    <span>2</span>
-                                </div>
-                            </div>                        
-
-                            <div class="item clearfix">
-                                <div class="image">
-                                    <a href="#"><img src="/assets/aquarius/img/users/no-image50.jpg" width="32"/></a>
-                                </div>
-                                <div class="info">
-                                    <a href="#" class="name">Survey</a>  
-                                    <span>11</span>
-                                </div>
-                            </div>                              
-                        
-                            <div class="item clearfix">
-                                <div class="image">
-                                    <a href="#"><img src="/assets/aquarius/img/users/no-image50.jpg" width="32"/></a>
-                                </div>
-                                <div class="info">
-                                    <a href="#" class="name">Install</a>                                    
-                                    <span>122</span>
-                                </div>
-                            </div>
-                            <div class="item clearfix">
-                                <div class="image">
-                                    <a href="#"><img src="/assets/aquarius/img/users/no-image50.jpg" width="32"/></a>
-                                </div>
-                                <div class="info">
-                                    <a href="#" class="name">Disconnect</a>
-                                    <span>1</span>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="footer">
-                            <button class="btn btn-danger link_bcPopupList" type="button">Close</button>
-                        </div>
-                    </div>
-                </li>                
-            </ul>
+            <?php $this->load->view("common/status");?>
         </div>
         
         <div class="workplace">
